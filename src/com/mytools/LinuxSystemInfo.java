9444e44250356a49b11005668aaf3ad6f7a6bc95
Erling Wulf Weinreich <hex316aa@gmail.com>
2025-03-30 19:50:27 +0800

    Updated linux system info tool

--- src/com/mytools/LinuxSystemInfo.java
+++ src/com/mytools/LinuxSystemInfo.java
@@ -3,6 +3,10 @@ package com.mytools;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,6 +14,9 @@ public class LinuxSystemInfo {
 
     public static void main(String[] args) {
         Map<String, String> systemInfo = new HashMap<>();
+
+        // Pixel Art and Author Information
+        displayAuthorInfo();
 
         // Basic System Information
         systemInfo.put("OS Name", System.getProperty("os.name"));
@@ -26,14 +33,33 @@ public class LinuxSystemInfo {
             systemInfo.put("Disk Space (Total)", executeCommand("df -h / | awk '/\\/dev\\// {print $2}'"));
             systemInfo.put("Kernel Version", executeCommand("uname -r"));
             systemInfo.put("Uptime", executeCommand("uptime -p"));
-
         } catch (IOException | InterruptedException e) {
             System.err.println("Error retrieving hardware information: " + e.getMessage());
+        }
+
+        // Network Information
+        try {
+            getNetworkInfo(systemInfo);
+        } catch (SocketException e) {
+            System.err.println("Error retrieving network information: " + e.getMessage());
         }
 
         // Display Information
         System.out.println("--- System Information ---");
         systemInfo.forEach((key, value) -> System.out.println(key + ": " + value));
+    }
+
+    private static void displayAuthorInfo() {
+        System.out.println("      .--.      ");
+        System.out.println("     |o_o |     ");
+        System.out.println("     |:_/ |     ");
+        System.out.println("    //   \\ \\    ");
+        System.out.println("   (|     |)    ");
+        System.out.println("  /'\\_   _/`\\  ");
+        System.out.println("  \\___)=(___/   ");
+        System.out.println("  Author: 0xWulf");
+        System.out.println("  Email: 0xwulf@proton.me");
+        System.out.println("  ----------------------------");
     }
 
     private static String executeCommand(String command) throws IOException, InterruptedException {
@@ -46,5 +72,27 @@ public class LinuxSystemInfo {
         }
         process.waitFor();
         return output.toString().trim();
+    }
+
+    private static void getNetworkInfo(Map<String, String> systemInfo) throws SocketException {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            if (!networkInterface.isVirtual() && networkInterface.isUp()) {
+                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress inetAddress = inetAddresses.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        systemInfo.put("Network Interface (" + networkInterface.getName() + ")", networkInterface.getDisplayName());
+                        systemInfo.put("IP Address (" + networkInterface.getName() + ")", inetAddress.getHostAddress());
+                        try {
+                            systemInfo.put("MAC Address (" + networkInterface.getName() + ")", executeCommand("ip link show " + networkInterface.getName() + " | awk '/link\\/ether/ {print $2}'"));
+                        } catch (IOException | InterruptedException e) {
+                            systemInfo.put("MAC Address (" + networkInterface.getName() + ")", "Unavailable");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
